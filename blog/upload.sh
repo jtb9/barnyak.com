2ef235e5032bd0e6435e73dd6a160a910ad2977c
Justin Barnyak <justin@innovationwithin.com>
2023-08-14 17:21:00 -0400

    content update

--- blog/upload.sh
+++ blog/upload.sh
@@ -3,17 +3,23 @@ TARGET_BUCKET='barnyak.com'
 
 echo "Running uploader...."
 
-while true; do
-    read -p "Your in the AWS environment ${AWS_PREFIX} are you sure you want to run this?" yn
-    case $yn in
-        [Yy]* ) aws s3 cp ./public s3://${TARGET_BUCKET}/ --recursive --acl public-read; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
+# while true; do
+#     read -p "Your in the AWS environment ${AWS_PREFIX} are you sure you want to run this?" yn
+#     case $yn in
+#         [Yy]* ) aws s3 cp ./public s3://${TARGET_BUCKET}/ --recursive --acl public-read; break;;
+#         [Nn]* ) exit;;
+#         * ) echo "Please answer yes or no.";;
+#     esac
+# done
 
-echo "Building cloudfront invalidation..."
+# echo "Building cloudfront invalidation..."
 
-aws cloudfront create-invalidation --distribution-id E1H7L6RMFQ0DR4 --paths '/*'
+# aws cloudfront create-invalidation --distribution-id E1H7L6RMFQ0DR4 --paths '/*'
+
+cd public
+
+zip build.zip -r ./*
+
+echo "Now manually deploy build.zip to cloudflare pages"
 
 echo "Done"
