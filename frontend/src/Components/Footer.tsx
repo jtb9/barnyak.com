7a77989e3b86390c477f23e57d97dc9adeef1ab5
Justin Barnyak <justin@barnyak.com>
2024-06-28 10:33:22 -0400

    mobile fixes

--- frontend/src/Components/Footer.tsx
+++ frontend/src/Components/Footer.tsx
@@ -1,28 +1,42 @@
 import { Stack } from "@mui/material";
 
 export default function Footer() {
-    return <div className="footer-spacer">
-        <footer id="footer">
+    return <Stack sx={{paddingTop: '20px', paddingBottom: '20px'}} direction="column" justifyContent={"center"}>
+        <Stack sx={{textAlign: 'center'}} direction="column" justifyContent="center">
+            <a target="_blank" rel="noopener" href="https://twitter.com/JustinBarnyak?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-size="large"
+                data-show-count="false">Follow @JustinBarnyak</a>
+        </Stack>
+        <Stack sx={{paddingTop: '10px'}} direction="column" justifyContent="center">
+            <div style={{ textAlign: 'center' }}>
+                Copyright &copy;
 
-            <span style={{
-                display: 'block',
-                margin: 'auto',
-                marginTop: '40px',
-                marginBottom: '10px',
-                width: '181px'
-            }}>
-                <a target="_blank" rel="noopener" href="https://twitter.com/JustinBarnyak?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-size="large"
-                    data-show-count="false">Follow @JustinBarnyak</a>
-                <script async src="https://platform.twitter.com/widgets.js"></script>
-            </span>
-            <Stack direction="column" justifyContent="center">
-                <div style={{textAlign: 'center'}}>
-                    Copyright &copy;
+                2018-2024
+                Justin Barnyak
+            </div>
+        </Stack>
+    </Stack>
+    // return <div className="footer-spacer">
+    //     <footer id="footer">
 
-                    2018-2024
-                    Justin Barnyak
-                </div>
-            </Stack>
-        </footer>
-    </div >
+    //         <span style={{
+    //             display: 'block',
+    //             margin: 'auto',
+    //             marginTop: '40px',
+    //             marginBottom: '10px',
+    //             width: '181px'
+    //         }}>
+    //             <a target="_blank" rel="noopener" href="https://twitter.com/JustinBarnyak?ref_src=twsrc%5Etfw" className="twitter-follow-button" data-size="large"
+    //                 data-show-count="false">Follow @JustinBarnyak</a>
+    //             <script async src="https://platform.twitter.com/widgets.js"></script>
+    //         </span>
+    //         <Stack direction="column" justifyContent="center">
+    //             <div style={{textAlign: 'center'}}>
+    //                 Copyright &copy;
+
+    //                 2018-2024
+    //                 Justin Barnyak
+    //             </div>
+    //         </Stack>
+    //     </footer>
+    // </div >
 }
