f6e1044292bca7d60a31e064f1fcfe41e8bef6c1
Justin Barnyak <justin@barnyak.com>
2024-07-02 10:00:07 -0400

    master: adding aptabase

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { AptabaseProvider } from '@aptabase/react';
 
 export const metadata: Metadata = {
   title: "Barnyak",
@@ -15,7 +16,7 @@ export default function RootLayout({
       <head>
       <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body>{children}</body>
+      <body><AptabaseProvider appKey="A-SH-0454767800">{children}</AptabaseProvider></body>
     </html>
   );
 }
