1a7b7262e58790261262389f0f250b824040f588
Justin Barnyak <hawksprite@gmail.com>
2025-04-12 08:27:58 -0400

    master: fixing matomo

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { AptabaseProvider } from '@aptabase/react';
 
 export const metadata: Metadata = {
   title: "Barnyak",
@@ -15,25 +14,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <link href="https://fonts.googleapis.com/css2?family=Crimson+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap" rel="stylesheet" />
-      <script>
-        {`
-        var _paq = window._paq = window._paq || [];
-        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-        _paq.push(['trackPageView']);
-        _paq.push(['enableLinkTracking']);
-        (function() {
-          var u="//analytics.barnyak.com/";
-          _paq.push(['setTrackerUrl', u+'matomo.php']);
-          _paq.push(['setSiteId', '3']);
-          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-          g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-        })();`}
-      </script>
 
       </head>
-      <body><AptabaseProvider options={{
-        host: 'https://analytics.barnyak.com'
-      }} appKey="A-SH-0454767800">{children}</AptabaseProvider></body>
+      <body>{children}</body>
     </html>
   );
 }
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,18 @@
 "use client";
+import { useEffect } from "react";
 import "./App.css";
 import Blog from "./Components/Blog";
 
 export default function Home() {
+  useEffect(() => {
+    //@ts-ignore
+    var _mtm = window._mtm = window._mtm || [];
+    _mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    //@ts-ignore
+    g.async=true; g.src='https://analytics.barnyak.com'; s.parentNode.insertBefore(g,s);
+}, [])
+
   return (
     <Blog />
   );
